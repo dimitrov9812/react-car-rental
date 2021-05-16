8dea38f9bc376514ccbba06c7426d596300dbcc1
Alexander Dimitrow <alexanderdimitrov333@gmail.com>
2021-05-16 15:38:21 +0300

    Uploading initial project structure. Finished setting up basic routing. Created the json-server db.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+            <li>
+              <Link to="/home">home</Link>
+            </li>
+            <li>
+              <Link to="/browse">browse</Link>
+            </li>
+            <li>
+              <Link to="/vehicle-details">vehicle-details</Link>
+            </li>
+            <li>
+              <Link to="/rentals/all">all rentals</Link>
+            </li>
+            <li>
+              <Link to="/rentals/mine">mine rentals</Link>
+            </li>
+            <li>
+              <Link to="/rentals/request">request rentals</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <div>login</div>
+          </Route>
+          <Route path="/home">
+            <div>home</div>
+          </Route>
+          <Route path="/browse">
+            <div>browse</div>
+          </Route>
+          <Route path="/vehicle-details">
+            <div>vehicle-details</div>
+          </Route>
+          <Route path="/rentals/all">
+            <div>all rentals</div>
+          </Route>
+          <Route path="/rentals/mine">
+            <div>mine rentals</div>
+          </Route>
+          <Route path="/rentals/request">
+            <div>request rental</div>
+          </Route>
+          <Route path="/">
+            <div>login</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
